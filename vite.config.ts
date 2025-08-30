b70e4ffb6830cf9db6870b29f07616869849b8c5
LamLong (Justin) <50525244+ngoclonglam@users.noreply.github.com>
2025-08-31 03:17:49 +0700

    Update base dist

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/green-basket-flow/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
